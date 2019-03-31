977a7f09ae9f7cd9b4b597155f77f7eeb7d42236
Vasani Divyesh INPU <u951438@syngenta.org>
2019-03-31 17:04:56 +0530

    Set up app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -108,7 +109,7 @@ app.get('*', (req, res) => {
     //res.send('404 page')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server up and running on port 3000.')
 })
 
